2b9c7504644f61a07da8f66f3f879034a56023d1
Binho <jose.rubens.veiga@gmail.com>
2022-03-11 20:27:27 -0300

    teste deploy

---
+++ IBook.API/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IBook.API.Controllers
+{
+    [Route("api/home")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Test()
+        {
+            return "teste";
+        }
+    }
+}
